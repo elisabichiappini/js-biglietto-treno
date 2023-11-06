dafdf4f16135a5090d81fc979288f19138bfb15e
elisabichiappini <elisabichiappini@gmail.com>
2023-11-06 21:06:29 +0100

    feat: in console prezzi con decimali scontati e no

--- js/myscript.js
+++ js/myscript.js
@@ -10,22 +10,44 @@ const etaInput = prompt ('Quanti hanni hai?');
 console.log(parseInt(etaInput));
 
 // 3) condizione:
+
+
 // SE etàInput < 18 { Stampo: ok sconto! e quindi (kmInput * 0.21€) - sconto 20%};
 if (etaInput < 18) {
     console.log('Hai diritto allo sconto minori 18');
-    const costoMinorenne = (kmInput * 0.2);
-    console.log(costoMinorenne);
+    const costoMinorenne = ((kmInput * 0.21) * 0.2);
+    const costoMinorenneDecimale2 = costoMinorenne.toFixed(2);
+    console.log(costoMinorenneDecimale2);
 }
 // ALTRIMENTI SE etàInput >= 65 { Stampo: ok sconto! e quindi (kmInput * 0.21€) - sconto 40%};
 else if (etaInput >= 65) {
     console.log('Hai diritto allo sconto senior65');
-    const costoSenior = (kmInput * 0.4);
-    console.log(costoSenior);
+    const costoSenior = ((kmInput * 0.21) * 0.4);
+    const costoSeniorDecimale2 = costoSenior.toFixed(2);
+    console.log(costoSeniorDecimale2);
 }
 // ALTRIMENTI { Stampo: no sconto! e quindi (kmInput * 0.21€)};
 else {
-    const prezzoPieno = (kmInput * 0.21);{
+    console.log('non hai diritto a nessuno sconto');
+    const prezzoPieno = (kmInput * 0.21);
     console.log(prezzoPieno);
-    }
 }
 // 4) prezzoUtenteFinale {massimo due decimali};
+
+
+// 5) in HTML modificare valore id=prezzobiglietto //errore in console
+
+let prezzoBiglietto = document.getElementById ('prezzobiglietto');
+prezzoBiglietto.innerHTML = 'Il prezzo del tuo biglietto è: ' + prezzoBiglietto ;
+
+if ((costoMinorenneDecimale2 >= 0) && (costoSeniorDecimale2 === 0) && (prezzoPieno === 0)) {
+    prezzoBiglietto.innerHTML = costoMinorenneDecimale2;
+}
+
+else if ((costoSeniorDecimale2 >= 0) && (costoMinorenneDecimale2 === 0) && (prezzoPieno === 0)) {
+    prezzoBigliett = costoSeniorDecimale2;
+}
+
+else {
+    prezzoBiglietto = prezzoPieno;
+};
