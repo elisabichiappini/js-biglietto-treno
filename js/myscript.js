8eafce73fa7b15fa80750f72e399db89c64e2556
elisabichiappini <elisabichiappini@gmail.com>
2023-11-06 17:25:05 +0100

    feat: condizione

--- js/myscript.js
+++ js/myscript.js
@@ -8,10 +8,24 @@ console.log(parseInt(kmInput));
 // 2) chiedere all'untente (input) quanti anni ha (numero) e salvare in una variabile;
 const etaInput = prompt ('Quanti hanni hai?');
 console.log(parseInt(etaInput));
+
 // 3) condizione:
 // SE etàInput < 18 { Stampo: ok sconto! e quindi (kmInput * 0.21€) - sconto 20%};
-
+if (etaInput < 18) {
+    console.log('Hai diritto allo sconto minori 18');
+    const costoMinorenne = (kmInput * 0.2);
+    console.log(costoMinorenne);
+}
 // ALTRIMENTI SE etàInput >= 65 { Stampo: ok sconto! e quindi (kmInput * 0.21€) - sconto 40%};
-
+else if (etaInput >= 65) {
+    console.log('Hai diritto allo sconto senior65');
+    const costoSenior = (kmInput * 0.4);
+    console.log(costoSenior);
+}
 // ALTRIMENTI { Stampo: no sconto! e quindi (kmInput * 0.21€)};
+else {
+    const prezzoPieno = (kmInput * 0.21);{
+    console.log(prezzoPieno);
+    }
+}
 // 4) prezzoUtenteFinale {massimo due decimali};
